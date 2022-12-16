d7fb1d971c8a39abc31562fe7203c80e77bfd7d7
Navid Haghdadi <n.haghdadi@unsw.edu.au>
2022-12-16 14:22:44 +1100

    Updating the code!

--- APIDataRead.py
+++ APIDataRead.py
@@ -10,44 +10,47 @@ def api_data_extractor(start_date,end_date,token, field="all",postcode="all"):
     df_load_all = pd.DataFrame()
 
     for date in date_range.values.astype('<M8[D]').astype(str):
-        response = requests.get("https://pv-map.apvi.org.au/api/v2/2-digit/{}.json?access_token={}".format(date, token))
-        print(date + " started")
+        try:
+            response = requests.get("https://pv-map.apvi.org.au/api/v2/2-digit/{}.json?access_token={}".format(date, token))
+            print(date + " started")
 
-        data = response.json()
+            data = response.json()
 
-        if field == "all" or field == "capacity":
-            df_cap = pd.DataFrame.from_dict(data['capacity'], orient='index')
-            df_cap.reset_index(inplace=True)
-            df_cap['Date'] = date
-            df_cap.columns = ['Postcode', 'Capacity_MW', 'Date']
-            df_cap = df_cap[['Date', 'Postcode', 'Capacity_MW']]
-            if postcode != "all":
-                df_cap = df_cap[df_cap['Postcode'].str.startswith(str(postcode))].copy()
-            df_cap_all = df_cap_all.append(df_cap)
+            if field == "all" or field == "capacity":
+                df_cap = pd.DataFrame.from_dict(data['capacity'], orient='index')
+                df_cap.reset_index(inplace=True)
+                df_cap['Date'] = date
+                df_cap.columns = ['Postcode', 'Capacity_MW', 'Date']
+                df_cap = df_cap[['Date', 'Postcode', 'Capacity_MW']]
+                if postcode != "all":
+                    df_cap = df_cap[df_cap['Postcode'].str.startswith(str(postcode))].copy()
+                df_cap_all = df_cap_all.append(df_cap)
 
-        if field == "all" or field == "performance":
-            df_per = pd.DataFrame.from_dict(data['performance'], orient='index')
-            df_per.reset_index(inplace=True)
-            df_per = pd.melt(df_per, id_vars=['index'])
-            df_per.columns = ['Postcode', 'Timestamp', 'Performance']
-            if postcode != "all":
-                df_per = df_per[df_per['Postcode'].str.startswith(str(postcode))].copy()
-            df_per_all = df_per_all.append(df_per)
+            if field == "all" or field == "performance":
+                df_per = pd.DataFrame.from_dict(data['performance'], orient='index')
+                df_per.reset_index(inplace=True)
+                df_per = pd.melt(df_per, id_vars=['index'])
+                df_per.columns = ['Postcode', 'Timestamp', 'Performance']
+                if postcode != "all":
+                    df_per = df_per[df_per['Postcode'].str.startswith(str(postcode))].copy()
+                df_per_all = df_per_all.append(df_per)
 
-        if field == "all" or field == "output":
-            df_out = pd.DataFrame.from_dict(data['output'], orient='index')
-            df_out.reset_index(inplace=True)
-            df_out = pd.melt(df_out, id_vars=['index'])
-            df_out.columns = ['Postcode', 'Timestamp', 'Output']
-            if postcode != "all":
-                df_out = df_out[df_out['Postcode'].str.startswith(str(postcode))].copy()
-            df_out_all = df_out_all.append(df_out)
+            if field == "all" or field == "output":
+                df_out = pd.DataFrame.from_dict(data['output'], orient='index')
+                df_out.reset_index(inplace=True)
+                df_out = pd.melt(df_out, id_vars=['index'])
+                df_out.columns = ['Postcode', 'Timestamp', 'Output']
+                if postcode != "all":
+                    df_out = df_out[df_out['Postcode'].str.startswith(str(postcode))].copy()
+                df_out_all = df_out_all.append(df_out)
 
-        if field == "all" or field == "load":
-            df_load = pd.DataFrame.from_dict(data['load'], orient='index')
-            df_load.reset_index(inplace=True)
-            df_load.rename(columns={'index': 'Timestamp'}, inplace=True)
-            df_load_all = df_load_all.append(df_load)
+            if field == "all" or field == "load":
+                df_load = pd.DataFrame.from_dict(data['load'], orient='index')
+                df_load.reset_index(inplace=True)
+                df_load.rename(columns={'index': 'Timestamp'}, inplace=True)
+                df_load_all = df_load_all.append(df_load)
+        except:
+            print("An exception occurred for "+ date)
     return [df_load_all, df_out_all, df_cap_all, df_per_all]
 
 
--- Example.py
+++ Example.py
@@ -1,22 +1,23 @@
 import pandas as pd
-import requests
-from Token import token
+# from Token import token
 from APIDataRead import api_data_extractor
-
 # examples:
 
 # specify the dates, the token needs to be imported as well
-start_date = "2020-01-01"
-end_date = "2020-01-03"
+token = ""  # use the token that was provided to you 
+start_date = "2022-12-01"
+end_date = "2022-12-03"
 
 # get all data:
-# [df_load_all, df_out_all, df_cap_all, df_per_all] = api_data_extractor(start_date, end_date, token)
+[df_load_all, df_out_all, df_cap_all, df_per_all] = api_data_extractor(start_date, end_date, token)
 
 # for specific field performance, capacity, load or output
-[df_load_all2, df_out_all2, df_cap_all2, df_per_all2] = api_data_extractor(start_date, end_date, token, field="capacity")
+# [df_load_all2, df_out_all2, df_cap_all2, df_per_all2] = api_data_extractor(start_date, end_date, token, field="performance")
 # saving data to csv
-df_cap_all2.to_csv("CapacityData_2019.csv", index=False)
+
+
 
 # # for specific postcode add the postcode in text, for example: "31" or "3" (to have all postcodes starting with 3)
 # [df_load_all, df_out_all, df_cap_all, df_per_all] = api_data_extractor(start_date, end_date, token, field="performance", postcode="31")
+
 
